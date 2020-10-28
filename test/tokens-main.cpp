31469a5b7a4b8cf934a1da7e354cd8f226482ef1
Gijs Van Laer <gijs.vanlaer@gmail.com>
2020-10-28 15:58:29 +0100

    EMP-14: use emp-ag2pc bare version

--- test/tokens-main.cpp
+++ test/tokens-main.cpp
@@ -16,6 +16,35 @@ int translate_initSHA256(bool *in, int pos) {
     return pos;
 }
 
+inline const char* hex_char_to_bin(char c) {
+    switch(toupper(c)) {
+        case '0': return "0000";
+        case '1': return "0001";
+        case '2': return "0010";
+        case '3': return "0011";
+        case '4': return "0100";
+        case '5': return "0101";
+        case '6': return "0110";
+        case '7': return "0111";
+        case '8': return "1000";
+        case '9': return "1001";
+        case 'A': return "1010";
+        case 'B': return "1011";
+        case 'C': return "1100";
+        case 'D': return "1101";
+        case 'E': return "1110";
+        case 'F': return "1111";
+        default: return "0";
+    }
+}
+
+inline std::string hex_to_binary(std::string hex) {
+    std::string bin;
+    for(unsigned i = 0; i != hex.length(); ++i)
+        bin += hex_char_to_bin(hex[i]);
+    return bin;
+}
+
 const string circuit_file_location = macro_xstr(EMP_CIRCUIT_PATH);
 void test(int party, NetIO* io, string name, string check_output = "") {
     // read in the circuit from the location where it was generated
